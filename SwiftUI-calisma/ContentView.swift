5b3d0785b6f977e3769dff153ea2ff65d69c0930
mustafamerttaskin-dev0 <mustafamerttaskin0@gmail.com>
2026-01-28 17:41:51 +0300

    Initial Commit

---
+++ SwiftUI-calisma/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-calisma
+//
+//  Created by Mustafa Mert Taşkın on 28.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-calisma/SwiftUI_calismaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_calismaApp.swift
+//  SwiftUI-calisma
+//
+//  Created by Mustafa Mert Taşkın on 28.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_calismaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
